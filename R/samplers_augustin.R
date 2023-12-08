cf50a14ee12ac147298093f99ef44aa7501c1711
DanielBonnery <daniel.bonnery@gmail.com>
2023-12-08 11:06:36 +0100

    maj compare.Rmd

--- R/samplers_augustin.R
+++ R/samplers_augustin.R
@@ -1,9 +1,10 @@
 compute_z<-function(beta){
-  z=1*(beta!=0)
+  z=1*((beta!=0)&!is.na(beta))
 }
 
 compute_beta<-function(z, beta_tilde){
-  z[z==1]<-beta_tilde
+  z[is.na(z)]<-0
+  z[z==1]<-beta_tilde[!is.na(beta_tilde)]
   z}
 
 sum_var <-function(X){
@@ -18,7 +19,7 @@ conditional_posterior<- function(R2, q, Y,U,X,sigma2,phi,beta,z,a,b,A,B){
   #z <- compute_z(beta)
   k <- ncol(X)
   v_X <- sum_var(X)
-  s_z <- sum(z)
+  s_z <- sum(z,na.rm=TRUE)
   res1 <- exp( -1/(2*sigma2)*(k*v_X*q*(1-R2))/R2 * t(beta) %*%  diag(z) %*% beta)
   res2 <- q^(s_z + 0.5*s_z + a -1)
   res3 <-(1-q)^(k-s_z + b -1 )
@@ -48,7 +49,7 @@ conditional_posterior<- function(R2, q, Y,U,X,sigma2,phi,beta,z,a,b,A,B){
 #'U=u=generate_u();
 #'barvx=barvx_f(xx)
 #'tbetabeta=t(beta)%*%beta
-#'s_z=sum(z)
+#'s_z=sum(z,na.rm=TRUE)
 #'r2_q_grid=r2_q_grid_generate()
 #'dan=sample_r2_q_cond_y_u_x_theta_z(sigma_epsilon,barvx,k,a,b,aa,bb,tbetabeta,s_z,r2_q_grid,m=m)
 #'plot(sort(dan[,1]),sort(aug[,1]));abline(0,1,col='red')
@@ -73,7 +74,7 @@ dist_conditional_posterior_R2_q<-function(Y,U,X,sigma2,phi,beta,z,a,b,A,B){
   list_prob_df<-data.frame(q=numeric(0),R2=numeric(0),prob=numeric(0))
   k <- ncol(X)
   v_X <- sum_var(X)
-  s_z <- sum(z)
+  s_z <- sum(z,na.rm=TRUE)
   calc_mat<- t(beta) %*%  diag(z) %*% beta
   for (i in 1:p){
     R2<-grid[i]
@@ -125,7 +126,7 @@ draw_conditional_posterior_R2_q<-function(sigma2,beta,z,a,b,A,B,v_X,k,s_z,grid_a
   grid_a|>
     dplyr::mutate(prob=likelihood_conditional_posterior_R2_q(R2,q,sigma2,beta,z,a,b,A,B,v_X,k,s_z,correction=TRUE),
                   prob=ifelse(is.na(prob),0,prob))|>
-    dplyr::slice(sample(dplyr::n(),size=m,prob=prob,replace=TRUE))|>
+    dplyr::slice(sample(dplyr::n(),size=m,prob=prob+(all(prob==0)),replace=TRUE))|>
     (`[`)(c("R2","q"))|>
     as.matrix()|>
     (`[`)(1:m,)
@@ -133,15 +134,7 @@ draw_conditional_posterior_R2_q<-function(sigma2,beta,z,a,b,A,B,v_X,k,s_z,grid_a
 
 
 def_X_tilde<-function(X,z){
-  X_tilde <- diag(0, nrow = nrow(X), ncol = sum(z))
-  cpt<-0
-  for (i in 1:ncol(X)) {
-    if (z[i]==1) {
-      cpt<-cpt+1
-      X_tilde[,cpt]<-X[, i]
-    }
-  }
-  return (X_tilde)
+  X[,is.na(z)|(z==1)]
 }
 
 
@@ -166,7 +159,7 @@ def_X_tilde<-function(X,z){
 #'U=u=generate_u();
 #'barvx=barvx_f(xx)
 #'tbetabeta=t(beta)%*%beta
-#'s_z=sum(z)
+#'s_z=sum(z,na.rm=TRUE)
 #'r2_q_grid=r2_q_grid_generate()
 #'q=mean(z)
 #'gamma2=gamma2_f(r2 = r2,k = k,q = q,barvx = barvx)
@@ -186,22 +179,25 @@ sample_conditional_posterior_zi<-function(Y,U,X,sigma2,phi,gamma2,q,z,i,m=1){
   
   
   z[i]<-0
-  s_z<-sum(z)
+  z[is.na(z)]<-0
+  s_z<-sum(z,na.rm=TRUE)
   if (s_z==0){
     return(1)
-  }
+  }else{
   #cas zi=0
-  I_sz_0 <- diag(1, nrow = sum(z), ncol = sum(z))
+  I_sz_0 <- diag(1, nrow = s_z, ncol = s_z)
   X_tilde_0<-def_X_tilde(X,z)
   W_tilde_0<-t(X_tilde_0)%*%X_tilde_0 + I_sz_0/gamma2
-  beta_tilde_hat_0<-solve(W_tilde_0)%*%t(X_tilde_0)%*%Y_tilde
+  beta_tilde_hat_0<-try(solve(W_tilde_0)%*%t(X_tilde_0)%*%Y_tilde)
+  if(is.element("try-error",class(beta_tilde_hat_0))){beta_tilde_hat_0=rep(0,nrow(Y_tilde))}
   proba_0<-((t(Y_tilde)%*%Y_tilde - t(beta_tilde_hat_0)%*%W_tilde_0%*%beta_tilde_hat_0) )
   
   
   #cas zi=1
   z[i]<-1
-  s_z<-sum(z)
-  I_sz_1 <- diag(1, nrow = sum(z), ncol = sum(z))
+  z[is.na(z)]<-0
+  s_z<-sum(z,na.rm=TRUE)
+  I_sz_1 <- diag(1, nrow = s_z, ncol = s_z)
   X_tilde_1<-def_X_tilde(X,z)
   W_tilde_1<-t(X_tilde_1)%*%X_tilde_1 + I_sz_1/gamma2
   beta_tilde_hat_1<-solve(W_tilde_1)%*%t(X_tilde_1)%*%Y_tilde
@@ -215,7 +211,7 @@ sample_conditional_posterior_zi<-function(Y,U,X,sigma2,phi,gamma2,q,z,i,m=1){
   #print(proba_1)
   #print(p)
   #print('fin')
-  return (rbinom(m, size = 1, prob = p))
+  return (rbinom(m, size = 1, prob = p))}
 }
 #'@examples
 #'a=default_a
@@ -238,7 +234,7 @@ sample_conditional_posterior_zi<-function(Y,U,X,sigma2,phi,gamma2,q,z,i,m=1){
 #'U=u=generate_u();
 #'barvx=barvx_f(xx)
 #'tbetabeta=t(beta)%*%beta
-#'s_z=sum(z)
+#'s_z=sum(z,na.rm=TRUE)
 #'r2_q_grid=r2_q_grid_generate()
 #'q=mean(z)
 #'gamma2=gamma2_f(r2 = r2,k = k,q = q,barvx = barvx)
@@ -282,7 +278,7 @@ sample_conditional_posterior_z<-function(Y,U,X,phi,R2,q,z0){
 #'U=u=generate_u();
 #'barvx=barvx_f(xx)
 #'tbetabeta=t(beta)%*%beta
-#'s_z=sum(z)
+#'s_z=sum(z,na.rm=TRUE)
 #'r2_q_grid=r2_q_grid_generate()
 #'q=mean(z)
 #'gamma2=gamma2_f(r2 = r2,k = k,q = q,barvx = barvx)
@@ -294,7 +290,7 @@ sample_conditional_posterior_z<-function(Y,U,X,phi,R2,q,z0){
 #'tilde_x=x[,z==1]
 #'X_tilde<-def_X_tilde(X,z)
 #'k_aug<-dim(X)[2]
-#'  I_sz <- diag(1, nrow = sum(z), ncol = sum(z))
+#'  I_sz <- diag(1, nrow = sum(z,na.rm=TRUE), ncol = sum(z,na.rm=TRUE))
 #'v_X <- sum_var(X)
 #'gamma2<-R2/(k*q*v_X*(1-R2))
 #'W_tilde<-t(X_tilde)%*%X_tilde + I_sz/gamma2
@@ -319,8 +315,9 @@ sample_conditional_posterior_z<-function(Y,U,X,phi,R2,q,z0){
 
 sample_conditional_posterior_sigma2<-function(Y,U,X,phi,R2, q, z,m=1){
   shape<-length(Y)/2 
-  I_sz <- diag(1, nrow = sum(z), ncol = sum(z))
+  I_sz <- diag(1, nrow = sum(z,na.rm=TRUE), ncol = sum(z,na.rm=TRUE))
   Y_tilde<- Y
+  z[is.na(z)]<-0
   X_tilde<-def_X_tilde(X,z)
   k<-dim(X)[2]
   v_X <- sum_var(X)
@@ -358,7 +355,7 @@ sample_conditional_posterior_sigma2<-function(Y,U,X,phi,R2, q, z,m=1){
 #'U=u=generate_u();
 #'barvx=barvx_f(xx)
 #'tbetabeta=t(beta)%*%beta
-#'s_z=sum(z)
+#'s_z=sum(z,na.rm=TRUE)
 #'r2_q_grid=r2_q_grid_generate()
 #'q=mean(z)
 #'gamma2=gamma2_f(r2 = r2,k = k,q = q,barvx = barvx)
@@ -370,7 +367,7 @@ sample_conditional_posterior_sigma2<-function(Y,U,X,phi,R2, q, z,m=1){
 #'tilde_x=x[,z==1]
 #'X_tilde<-def_X_tilde(X,z)
 #'k_aug<-dim(X)[2]
-#'  I_sz <- diag(1, nrow = sum(z), ncol = sum(z))
+#'  I_sz <- diag(1, nrow = sum(z,na.rm=TRUE), ncol = sum(z,na.rm=TRUE))
 #'v_X <- sum_var(X)
 #'gamma2<-R2/(k*q*v_X*(1-R2))
 #'W_tilde<-t(X_tilde)%*%X_tilde + I_sz/gamma2
@@ -402,7 +399,8 @@ sample_conditional_posterior_sigma2<-function(Y,U,X,phi,R2, q, z,m=1){
 
 
 sample_conditional_posterior_beta_tilde<-function(Y,U,X,phi,R2,q,sigma2,z,m=1){
-  I_sz <- diag(1, nrow = sum(z), ncol = sum(z))
+  z[is.na(z)]<-0
+  I_sz <- diag(1, nrow = sum(z,na.rm=TRUE), ncol = sum(z,na.rm=TRUE))
   Y_tilde<- Y
   X_tilde<-def_X_tilde(X,z)
   k<-dim(X)[2]
@@ -447,7 +445,7 @@ Gibbs<-function(N,a,A,b,B,k,U,phi,X,Y){
   sigma2<-init$sigma2
   phi<-0
   z <- compute_z(beta)
-  s_z<-sum(z)
+  s_z<-sum(z,na.rm=TRUE)
   list_R2 <- vector(mode="numeric", length=N)
   list_q <- vector(mode="numeric", length=N)
   for(i in 1:N){
@@ -458,7 +456,7 @@ Gibbs<-function(N,a,A,b,B,k,U,phi,X,Y){
     R2<-mat_R2q["R2"]
     q<-mat_R2q["q"]
     z<-sample_conditional_posterior_z(Y,U,X,phi,R2,q,z)
-    s_z<-sum(z)
+    s_z<-sum(z,na.rm=TRUE)
     sigma2<-sample_conditional_posterior_sigma2(Y,U,X,phi,R2, q, z)
     beta_tilde<-sample_conditional_posterior_beta_tilde(Y,U,X,phi,R2,q,sigma2,z)
     beta<-compute_beta(z,beta_tilde)
